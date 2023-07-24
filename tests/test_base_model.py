a4c523e2f2fb103cfa644abad36268f3460cd4aa
CyrineZekri <cyrinezekri4@gmail.com>
2023-07-24 13:40:13 +0100

    pycdoe styem

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
+from models.base_model import BaseModel
 import sys
 import os
 current_file_dir = os.path.dirname(os.path.abspath(__file__))
 project_folder = os.path.abspath(os.path.join(current_file_dir, '..'))
 sys.path.append(project_folder)
 
-from models.base_model import BaseModel
 
 my_model = BaseModel()
 my_model.name = "My First Model"
@@ -17,4 +17,6 @@ my_model_json = my_model.to_dict()
 print(my_model_json)
 print("JSON of my_model:")
 for key in my_model_json.keys():
-    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
+    print("\t{}: ({}) - {}".format(key,
+                                   type(my_model_json[key]),
+                                   my_model_json[key]))
