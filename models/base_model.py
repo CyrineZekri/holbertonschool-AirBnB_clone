a607204a3f42bb4a80f9c88904716e82886497eb
CyrineZekri <cyrinezekri4@gmail.com>
2023-07-24 12:39:23 +0100

    removing comments

--- models/base_model.py
+++ models/base_model.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """
+
 module that defines the Base Model
 """
 import uuid
@@ -12,15 +13,12 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """Instanciation Method"""
         if kwargs:
-            """If user adds other attributes"""
             for key, value in kwargs.items():
-                """ In Case the user wants to set updated_at or created_at"""
                 if key == 'created_at' or key == 'updated_at':
                     setattr(
                         self, key, datetime.strptime(
                             value, "%Y-%m-%dT%H:%M:%S.%f"))
                 elif key != '__class__':
-                    """To ensure user only passes valid arguments"""
                     setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
@@ -38,7 +36,6 @@ class BaseModel:
     def to_dict(self):
         """converts an instance to a dictionary"""
         obj_dict = self.__dict__.copy()
-        """ To isolate from the data stored in self.__dict__"""
         obj_dict["__class__"] = self.__class__.__name__
         obj_dict["created_at"] = self.created_at.isoformat()
         obj_dict["updated_at"] = self.updated_at.isoformat()
