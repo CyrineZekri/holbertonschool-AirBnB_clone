07e6b6cb37158e15ae8b4b00e97f9f2cb85cdb67
CyrineZekri <cyrinezekri4@gmail.com>
2023-07-24 19:17:53 +0100

    test model with path

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 import sys
 import os
-current_file_dir = os.path.dirname(os.path.abspath(__file__))
-project_folder = os.path.abspath(os.path.join(current_file_dir, '..'))
+#get the current file's directory path
+current_location=os.path.dirname(os.path.abspath(__file__))
+#append to it .. in order to get the whole project directory
+project_folder=os.path.abspath(os.path.join(current_location,'..'))
+#add this path to sys.path 
 sys.path.append(project_folder)
 from models.base_model import BaseModel
-
-
 my_model = BaseModel()
 my_model.name = "My First Model"
 my_model.my_number = 89
@@ -17,6 +18,4 @@ my_model_json = my_model.to_dict()
 print(my_model_json)
 print("JSON of my_model:")
 for key in my_model_json.keys():
-    print("\t{}: ({}) - {}".format(key,
-                                   type(my_model_json[key]),
-                                   my_model_json[key]))
+    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
