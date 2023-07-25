1a7619aeccf35d8a5954466a656797696a157a3f
CyrineZekri <cyrinezekri4@gmail.com>
2023-07-25 19:50:13 +0100

    file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+    #!/usr/bin/python3
 """ module  that serializes and deserializes instances """
 import json
 class FileStorage:
@@ -6,20 +6,23 @@ class FileStorage:
     __objects={}
     def all(self):
         """ returns all objects"""
-        return FileStorage.__objects
+        return self.__objects
     def new(self,obj):
         """adds a new object to the __objects() dict"""
         key=f"{obj.__class__.__name__}.{obj.id}"
-        FileStorage.__objects[key]=obj
+        self.__objects[key]=obj
     def save(self):
         """serializes the objects"""
-        with open(FileStorage.__file_path, "w") as json_file:
-            json.dump(FileStorage.__objects, json_file)
+        new_dict={}
+        for key, obj in self.__objects.items():
+            new_dict[key]=obj.to_dict()
+        with open(FileStorage.__file_path, "w") as f:
+            json.dump(FileStorage.__objects,f)
     def reload(self):
         """deserializes objects"""
         try:
             #do nthg if the file doesnt exist
-            with open(FileStorage.__file_path,"r") as json_file: 
-                FileStorage.__objects=json.load(json_file)
+            with open(FileStorage.__file_path,"r") as f: 
+                FileStorage.__objects=json.load(f)
         except FileNotFoundError:
             pass 
