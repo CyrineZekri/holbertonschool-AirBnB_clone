61dab55e383656ac8dfa6104c7fa8b435d57d4b3
CyrineZekri <cyrinezekri4@gmail.com>
2023-07-27 21:17:07 +0100

    working console

--- console.py
+++ console.py
@@ -2,15 +2,20 @@
 """the console module"""
 import cmd
 class HBNBCommand(cmd.Cmd):
-    prompt="(hbnb)"
-    def do_quit(self,arg):
-        """Exit the command interpreter"""
+    """Class of HBNBCommand"""
+
+    prompt = "(hbnb) "
+
+    def do_quit(self, arg):
+        """exiting the file"""
         return True
-    def do_EOF(self,arg):
-        """Exit the Command interpreter"""
+
+    def do_EOF(self, arg):
+        """Exiting the file"""
         return True
+
     def emptyline(self):
-        """when empty line, do nthg"""
+        """When empty line, do nthg"""
         pass
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
